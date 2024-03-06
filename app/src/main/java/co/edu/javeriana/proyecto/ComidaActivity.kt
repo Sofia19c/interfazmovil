0b5342e9d3fa80d0ba70d7839c96c2925b4e999d
sofia19c <sofivampaireknight@gmail.com>
2024-03-05 22:47:31 -0500

    hice nueva pantalla

--- app/src/main/java/co/edu/javeriana/proyecto/ComidaActivity.kt
+++ app/src/main/java/co/edu/javeriana/proyecto/ComidaActivity.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import co.edu.javeriana.proyecto.databinding.ActivityComidaBinding
 import co.edu.javeriana.proyecto.databinding.ActivityIngresarUserBinding
-class IngresarUserActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityIngresarUserBinding
 
+class ComidaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityComidaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityIngresarUserBinding.inflate(layoutInflater)
+        binding = ActivityComidaBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.botoningreso.setOnClickListener {
+        binding.perfilboton.setOnClickListener {
             Log.i("Hola","Presioné el boton")
-            val intento1 = Intent(this, ComidaActivity::class.java)
+            val intento1 = Intent(this, AjustesActivity::class.java)
             startActivity(intento1)
         }
     }
